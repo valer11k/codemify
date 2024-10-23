8c4acd94755dddcb18aef4c783852d72ba7280c8
Valeriia <1valeria.kozak@gmail.com>
2024-10-23 13:25:58 -0700

    Complete task 5

---
+++ week-3-js-algorithms/task5.js
@@ -0,0 +1,15 @@
+// Finish the function. It should accept one parameter, 'number', check if it is greater than 5 and return true or false.
+function greaterThen (number) {
+    if (number > 5){
+    return true;
+} else {
+    return false;
+}
+}
+
+//const result = greaterThen(2);
+//console.log(result); 
+console.log(greaterThen(1)); 
+
+
+
