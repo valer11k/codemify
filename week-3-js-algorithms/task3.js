9137105e3d8cac7601aae77bd61de19280fe4d17
Valeriia <1valeria.kozak@gmail.com>
2024-10-23 12:19:32 -0700

    Complete task

---
+++ week-3-js-algorithms/task3.js
@@ -0,0 +1,11 @@
+// Create a function called divideByTwo. It should accept 1 parameter, divide it  by 2 and return the result of the division
+function divideByTwo(numberToDivide) {
+    return numberToDivide / 2;
+  }
+  
+console.log(divideByTwo(6)) // 3 
+console.log(divideByTwo(105)) // Results in 52.5
+  
+/* FYI. Same thing can be achieved by assigning result to const first
+const result = divideByTwo(6)
+console.log(result) // 3 */
