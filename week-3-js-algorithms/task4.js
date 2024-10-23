671759a2b03bd780d96eb156e8f8d88070c0a92d
Valeriia <1valeria.kozak@gmail.com>
2024-10-23 12:49:48 -0700

    complete task 4

---
+++ week-3-js-algorithms/task4.js
@@ -0,0 +1,6 @@
+//Finish the function. It accepts 2 params, adds them up and returns the sum of two
+function addParameters(firstParameter, secondParameter) {
+    return firstParameter + secondParameter
+};
+const result = addParameters(5,4);
+console.log(result);
