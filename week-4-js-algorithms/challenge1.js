d8a01ce1330837aeebff3eb60f535aa74c67b7c9
Valeriia <1valeria.kozak@gmail.com>
2024-10-31 14:29:05 -0700

    complete task1

--- week-4-js-algorithms/challenge1.js
+++ week-4-js-algorithms/challenge1.js
@@ -1,6 +1,15 @@
 // Challenge 1: Find the Longest String
 // Write a function to find the longest string in the array.
+function findLongestString(array) {
+  let longest = "";
+  for (let index = 0; index < array.length; index++) {
+    if (array[index].length > longest.length) {
+      longest = array[index];
+    }
+  }
+  return longest;
+}
 // Usage examples:
-// console.log(findLongestString(["I", "am", "learning", "JavaScript"])); // "JavaScript"
-// console.log(findLongestString(["one", "two", "three"])); // "three"
-// console.log(findLongestString(["1", "22", "333"])); // "333"
+console.log(findLongestString(["I", "am", "learning", "JavaScript"])); // "JavaScript"
+console.log(findLongestString(["one", "two", "three"])); // "three"
+console.log(findLongestString(["1", "22", "333"])); // "333"
