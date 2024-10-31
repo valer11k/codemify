52f8ef1ec994a1dc5ba9dad9d1de8251d9a651e3
Valeriia <1valeria.kozak@gmail.com>
2024-10-31 14:41:09 -0700

    change

--- week-4-js-algorithms/challenge2.js
+++ week-4-js-algorithms/challenge2.js
@@ -1,12 +1,13 @@
 // Challenge 2: Find the Largest Number
 // Write a function to find the largest number in the array.
-function findLargestNumber(array){
-    let largestNumber = "";
-    for (let index = 0; index < array.length; index ++){
-        if(array[index] > largestNumber){
-            largestNumber = array[index]
-        }
-    } return largestNumber;
+function findLargestNumber(array) {
+  let largestNumber = array[0];
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] > largestNumber) {
+      largestNumber = array[index];
+    }
+  }
+  return largestNumber;
 }
 // Usage examples:
 console.log(findLargestNumber([1, 2, 3, 4, 5])); // 5
