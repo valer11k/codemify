d37378c6a0dc33e7016cbe9d7166b139ef1827aa
Valeriia <1valeria.kozak@gmail.com>
2024-11-04 12:26:06 -0800

    add files

---
+++ week-5-js-algorithms/helper.methods.js
@@ -0,0 +1,27 @@
+class Helpers {
+    getUsersByAge(response, ageMin, ageMax) {
+        const users = [];
+
+        // Your code here
+
+        return users;
+    }
+
+    getUsersByRole(response, role) {
+        const users = [];
+
+        // Your code here
+
+        return users;
+    }
+
+    getUsersByCountry(response, country) {
+        const users = [];
+
+        // Your code here
+
+        return users;
+    }
+}
+
+export default new Helpers()
