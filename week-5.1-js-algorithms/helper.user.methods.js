1c95f5fbe17975b92d24127644e362b073a15717
Valeriia <1valeria.kozak@gmail.com>
2024-11-06 13:23:14 -0800

    add files

---
+++ week-5.1-js-algorithms/helper.user.methods.js
@@ -0,0 +1,35 @@
+class Helpers {
+    getUsersByStatus(response, status) {
+        const users = [];
+
+        // Your code here
+
+        return users;
+    }
+
+    getUsersByRole(response, role, roleId) {
+        const users = [];
+
+        // Your code here
+
+        return users;
+    }
+
+    getUsersByEmailDomain(response, domain) {
+        const users = [];
+
+        // Your code here
+
+        return users;
+    }
+
+    getUserBalanceByCurrency(response, currency) {
+        const balances = [];
+
+        // Your code here
+
+        return balances;
+    }
+}
+
+export default new Helpers()
