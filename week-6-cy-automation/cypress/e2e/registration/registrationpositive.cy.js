09faeb45b8c7c8e981c84fa33d793cc037e6e498
Valeriia Kozak <valeriiakozak@Valeriias-MacBook-Air.local>
2024-11-12 17:46:45 -0800

    week-6-cy-automation

---
+++ week-6-cy-automation/cypress/e2e/registration/registrationpositive.cy.js
@@ -0,0 +1,35 @@
+import { faker } from "@faker-js/faker";
+
+const email = faker.internet.email();
+const password = faker.string.uuid();
+
+describe("Registration", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("Should register a new user account", () => {
+    cy.get('[href="/auth/register"]').click();
+    cy.get('[name="firstName"]').type("Valeriia");
+    cy.get('[name="lastName"]').type("Test");
+    cy.get('[name="email"]').type(email);
+    cy.get('[name="password"]').type(password);
+    cy.get('[type="submit"]').click();
+
+    cy.get("a p").should("have.text", "role: user");
+    cy.get("a h6").should("have.text", "Valeriia  Test");
+    cy.title().should("eq", "User: Profile | Delek Homes");
+    cy.url().should("include", "/dashboard/user/profile");
+
+    //Click user icon and logout button
+    cy.get('button [data-testid="PersonIcon"]').click();
+    cy.contains("Logout").click();
+
+    cy.get('[name="email"]').type(email);
+    cy.get('[name="password"]').type(password);
+    cy.contains("Login").click();
+
+    cy.get("a h6").should("have.text", "Valeriia  Test");
+    cy.get("a p").should("have.text", "role: user");
+  });
+});
