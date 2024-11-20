e580803145eaaa762629a358002912cb7be7ddf4
Valeriia Kozak <valeriiakozak@Valeriias-MacBook-Air.local>
2024-11-20 10:23:54 -0800

    week-6-cy-automation

--- week-7-cy-automation/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: "https://delekhomes.com",
-    viewportWidth: 1400,
-    viewportHeight: 1200,
-  },
-});
