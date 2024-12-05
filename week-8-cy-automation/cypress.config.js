2c4a2ab338beb6df6e2e81e1742cb43903ec286f
Valeriia Kozak <valeriiakozak08@gmail.com>
2024-12-05 14:33:32 -0800

    fix config

--- week-8-cy-automation/cypress.config.js
+++ week-8-cy-automation/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://delekhomes.com",
+    baseUrl: "https://dev.delekhomes.com",
     viewportWidth: 1400,
     viewportHeight: 1200,
   },
